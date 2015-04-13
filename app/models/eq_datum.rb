95af10d26bcae274da7365d770298e8e0bd9f84c
SOUbutou15@yahoo.co.jp <butou15@yahoo.co.jp>
2015-04-13 20:46:21 +0900

    装備追加 EqDatum編集用コントローラを作成

--- app/models/eq_datum.rb
+++ app/models/eq_datum.rb
@@ -1,6 +1,6 @@
 class EqDatum < ActiveRecord::Base
 	def view_name_and_scores
-		self.name+"( "+self.score.to_s+" => "+(self.score*self.rate).to_s+" )"
+		self.name+"( "+self.score.to_s+" => "+(self.score*self.rate).round(2).to_s+" )"
 	end
 
 end
